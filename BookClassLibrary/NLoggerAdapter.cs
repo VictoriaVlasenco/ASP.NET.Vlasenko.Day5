1f13736531b66c4a1fb28d51d0868c150502f4c1
Victoria Vlasenco <victoria.vlasenco@gmail.com>
2015-07-27 16:57:50 +0300

    Adapter added

---
+++ BookClassLibrary/NLoggerAdapter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookClassLibrary
+{
+    public class NLoggerAdapter : ILogger
+    {
+        private static readonly NLog.Logger nLogger = NLog.LogManager.GetCurrentClassLogger();
+
+        public void Debug(string message)
+        {
+            nLogger.Debug(message);
+        }
+
+        public void Warn(string message)
+        {
+            nLogger.Warn(message);
+        }
+
+        public void Error(string message)
+        {
+            nLogger.Error(message);
+        }
+
+        public void Info(string message)
+        {
+            nLogger.Info(message);
+        }
+    }
+}
